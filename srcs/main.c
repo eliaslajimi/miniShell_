cd0445194418cd6631be3ad777731e6e96cee813
cmcgahan <chmcgahan@gmail.com>
2020-12-15 18:08:58 +0100

    main_env

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,10 @@
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
+	g_env = setEnv(envp);
+	char *test = env_builtin("");
+	printf("bitebite	[%s]\n", test);
 	minishell();
 	return (0);
 }
