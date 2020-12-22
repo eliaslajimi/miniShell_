b051839e8ec80f8e7e338d6d4ffaf67f68f10db8
cmcgahan <chmcgahan@gmail.com>
2020-12-22 16:28:30 +0100

    pwd

--- srcs/pwd.c
+++ srcs/pwd.c
@@ -1,18 +1,14 @@
 #include "minishell.h"
 
-char	*pwd(int bufsize)
+char	*pwd()
 {
+	int		bufsize;
 	char	*buf;
 	char	*currentdir;
 
 	buf = NULL;
 	if ((currentdir = getcwd(buf, bufsize)) == NULL)
-	{
-		//errno = ERANGE 		How to set this properly?
-		printf("mooooore buff sizzzzze\n");
-		bufsize += 100;
 		pwd(bufsize);
-	}
 	free(buf);
 	return (currentdir);
 }
