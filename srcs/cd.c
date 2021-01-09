fe9031fc29ec022f5bbd3779084ea009b20babd6
Elias Lajimi <eliaslajimi@gmail.com>
2021-01-09 18:22:46 +0100

    fixing tild and error message for cd

--- srcs/cd.c
+++ srcs/cd.c
@@ -9,6 +9,11 @@ char *formatpath(char *path)
 		path[ft_strlen(path)] = 0;
 	if (!ft_strcmp(path, "-"))
 		path = cleannode(find_node("OLDPWD"));
+	if ((!ft_strcmp(path, "~")) || (!ft_strncmp(path, "~/", 2)))
+	{
+		path++;
+		path = ft_strjoin(ft_strjoin(ft_strdup(cleannode(find_node("HOME"))), "/"), path + 1);
+	 }
 	return (path);
 }
 
@@ -30,8 +35,18 @@ int applycmd(char *cmd)
 
 int argtocmd(char *arg)
 {
+	int *status;
+
+	status = (int*)getglobal(STATUS);
 	arg = formatpath(arg);
-	applycmd(arg);
+	*status = applycmd(arg);
+	if (*status != 0)
+	{
+		print("cd: no such file or directory: ", 1);
+		print(arg, 1);
+		print("\n", 1);
+	 }
+
 	return (0);
 }
 
@@ -47,7 +62,7 @@ int cd(char **args, int in, int out)
 		argtocmd("");
 	while (*args)
 	{
-		argtocmd(*args);
+	    argtocmd(*args);
 		args++;
 	}
 	return (status);
