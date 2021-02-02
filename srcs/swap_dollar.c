892c767569cef0e4822dcc6bad756a53be565452
Elias Lajimi <eliaslajimi@gmail.com>
2021-02-02 10:36:35 +0100

    pushing handlspace

--- srcs/swap_dollar.c
+++ srcs/swap_dollar.c
@@ -1,5 +1,29 @@
 #include "minishell.h"
 
+char    *handlspace(char *str)
+{
+        int i = 0;
+        int j = 0;
+        char *result;
+
+        result = malloc(sizeof(ft_strlen(str) + 1));
+        while(str[i])
+        {
+                if (str[i] == ' ')
+                {
+                        result[j] = ' ';
+                        j++;
+                        while (str[i] == ' ')
+                                i++;
+                }
+                result[j] = str[i];
+                j++;
+                i++;
+        }
+        result[j] = 0;
+        return (result);
+}
+
 char	*swap_dollar(char *s, int i, int len)
 {
 	//printf("trying to swap dollar\n");
@@ -12,10 +36,9 @@ char	*swap_dollar(char *s, int i, int len)
 	{
 		result = ft_strdup(find_node(tofind) + ft_strlen(tofind) + 1);
 		tmp = result;
-		result = ft_strtrim(result, " ");	
+		result = ft_strtrim(result, " ");
+		//result = handlspace(tmp);
 		free(tmp);
-		//printf("result is %s\n", result);
-		//printf("tofind is %s\n", );
 		ft_strdel(&tofind);
 		return (result);
 	}
